df25e4940c7cfdcce708e19adb57331ae3e631c9
arkebi24 <riteshbehera123@gmail.com>
2024-09-16 08:33:57 +0530

    Redirect directly to /app

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,5 @@
 import Dashboard from "@/pages/Dashboard";
-import LandingPage from "@/pages/LandingPage";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import SocialMediaResults from "@/pages/SocialMediaResults";
 import { ConfigurationPage } from "@/pages/ConfigurationPage";
 import PrivateRoute from "./PrivateRoute";
@@ -8,7 +7,7 @@ import PrivateRoute from "./PrivateRoute";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <LandingPage />,
+    element: <Navigate to="/app" />,
   },
   {
     path: "/app",
